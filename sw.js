3987f5411af7744212365d82ac313bb052423f75
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-04 14:01:51 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('fetch', function (event) {});
-const status = await navigator.permissions.query({
-  name: 'periodic-background-sync',
-});
-if (status.state === 'granted') {
-  // Periodic background sync can be used.
-} else {
-  // Periodic background sync cannot be used.
-}
