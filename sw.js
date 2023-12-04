4e0ae945ae013e4b1cb7d74052d9f8e67bcae3bb
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-04 13:42:17 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Service worker file (sw.js)
+
+const cacheName = 'my-pwa-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  
+  // Add other essential files and assets here
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
