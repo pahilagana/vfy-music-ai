a3cd15e8c3958688869d37d4544bdef83fb024aa
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-04 13:53:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,9 @@
-// Service worker file (sw.js)
-
-const cacheName = 'my-pwa-cache-v1';
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/offline',
-  
-  // Add other essential files and assets here
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('fetch', function (event) {});
+const status = await navigator.permissions.query({
+  name: 'periodic-background-sync',
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+if (status.state === 'granted') {
+  // Periodic background sync can be used.
+} else {
+  // Periodic background sync cannot be used.
+}
